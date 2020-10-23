d3614c351e2140f082ef1b3f2556508b94841fdf
zhiqiang <zhang@zhiqiang.org>
2020-10-23 13:12:08 +0800

    build

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=/opt/  -DQUILL_FMT_EXTERNAL=ON -DCMAKE_PREFIX_PATH=/opt/lib/cmake/fmt/ -DCMAKE_POSITION_INDEPENDENT_CODE=TRUE
+make install 
