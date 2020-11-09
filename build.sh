d00d60c325145a4960acf1356a4e16907fda088a
zhiqiang <zhang@zhiqiang.org>
2020-11-09 08:36:59 +0800

    small

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm build -rf
-mkdir build
-cd build
+rm .build -rf
+mkdir .build
+cd .build
 cmake .. -DCMAKE_INSTALL_PREFIX=/opt/  -DQUILL_FMT_EXTERNAL=ON -DCMAKE_PREFIX_PATH=/opt/lib/cmake/fmt/ -DCMAKE_POSITION_INDEPENDENT_CODE=TRUE
 make install 
